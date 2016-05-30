9f81509cb9a2d5d0718c87fb45c8f80ea99ec351
it270489bso <stanislav.berdnik@privatbank.ua>
2016-05-30 14:31:43 +0300

    new config definition

--- src/main/java/bso/berdnik/App.java
+++ src/main/java/bso/berdnik/App.java
@@ -5,6 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/springBeans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/application-context.xml");
     }
 }
